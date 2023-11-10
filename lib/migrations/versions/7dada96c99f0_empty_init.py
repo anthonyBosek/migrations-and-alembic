e80908cc26e7a040c3113d39a9062b48662daf8f
Anthony Bosek <anthony.bosek@gmail.com>
2023-11-10 15:45:46 -0700

    complete lab

---
+++ lib/migrations/versions/7dada96c99f0_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 7dada96c99f0
+Revises: 
+Create Date: 2023-11-10 15:32:22.228731
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '7dada96c99f0'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
